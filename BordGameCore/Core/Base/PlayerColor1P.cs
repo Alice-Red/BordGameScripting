7d5645e184c09d0a22adeb1de43a7a28fb475601
Alice-Red <redkun1014@gmail.com>
2019-10-01 20:08:43 +0900

    10/01_20:08

---
+++ BordGameCore/Core/Base/PlayerColor1P.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLib.Core.Base
+{
+    public enum PlayerColor1P
+    {
+        None = 0,
+        PL = 1,
+        Disable = 2,
+    }
+}
