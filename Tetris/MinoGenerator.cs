f6237edce15476ffd835dadc727a8385c442b301
樹 小田 <kaz_12@outlook.jp>
2019-10-14 18:27:37 +0900

    10/14_18:27

--- Tetris/MinoGenerator.cs
+++ Tetris/MinoGenerator.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RUtil;
 
 namespace Tetris
 {
     public class MinoGenerator
     {
-        private Queue<int> nexts = new Queue<int>();
-        public 
+
+        private int[] buffer = new int[0];
+        private int[] Buffer {
+            get {
+                if (buffer.Length == 0) 
+                    buffer = Enumerable.Range(1, 7).ToArray();
+                return buffer;
+            }
+        }
+
+        private Queue<int> _nexts;
+        private Queue<int> nexts {
+            get { }
+            set {
+
+            }
+        }
+
+
+        public Mino[] Nexts => nexts.Select(s => s.ToEnum<Mino>()).ToArray();
+        public Mino Current;
+
+        public MinoGenerator(int nextcount) {
+        }
 
         public Mino Generate() {
 
         }
-        
+
 
     }
 }
