0f963cb502e30c8ea6e1050715eb5cca351c89e4
Alice-Red <redkun1014@gmail.com>
2020-02-04 13:27:08 +0900

    02/04_13:26

--- BordGameCore/GameAutoLoop.cs
+++ BordGameCore/GameAutoLoop.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GameLib.API;
 
 namespace GameLib
 {
     public class GameAutoLoop
     {
-
+        GameInputter[] Players;
 
 
         public GameAutoLoop() {
