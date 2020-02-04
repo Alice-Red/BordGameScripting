8b1030dc27eb96670332222f44b984f7c50433f4
樹 小田 <kaz_12@outlook.jp>
2020-02-04 15:02:31 +0900

    02/04_15:02

---
+++ BordGameCore/Core/GameAutoLoop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameLib.API;
+
+namespace GameLib.Core
+{
+    public class GameAutoLoop
+    {
+
+        Game Target;
+        GameInputter[] Players;
+        int count;
+
+        public GameAutoLoop(Game target, GameInputter[] players, int count) {
+            Target = target;
+            Players = players;
+            this.count = count;
+        }
+
+        public void Boot() {
+            var GAME = (Target.GetType()) Activator.CreateInstance(Target.GetType());
+        }
+
+
+    }
+}
